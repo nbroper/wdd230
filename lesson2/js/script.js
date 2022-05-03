5d977ea31f9caf9b4a5814aa0dc9581d42b3f6ce
nbroper <nbroper@gmail.com>
2022-05-03 07:16:58 -0600

    monday night

---
+++ lesson2/js/script.js
@@ -0,0 +1,7 @@
+//current year after "copyright" symbol in footer
+const year = { year: 'numeric' };
+document.getElementById("currentYear").textContent = new Date().toLocaleDateString('en-US', year);
+
+//last updated date in footer
+const lastModified = document.lastModified;
+document.getElementById("modified").innerHTML = lastModified;
